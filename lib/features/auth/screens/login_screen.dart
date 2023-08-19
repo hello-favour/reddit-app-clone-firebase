997b5fb3e048964e3458439e327e099f5f53731b
Favour <hello.williamsfav@gmail.com>
2023-08-18 19:04:09 -0700

    build login screen

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_app_clone/components/sign_in_button.dart';
+import 'package:reddit_app_clone/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constant.logoPath,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              "Skip",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text(
+            "Dive into anything",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              letterSpacing: 0.5,
+            ),
+          ),
+          const SizedBox(height: 35),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              Constant.loginEmotePath,
+              height: 400,
+            ),
+          ),
+          const SizedBox(height: 25),
+          const SignInButton(),
+        ],
+      ),
+    );
   }
 }
